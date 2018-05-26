f24dd502ce1c796b084e412f3584472d5551c714
Richard Melchior <raustin.melchior@gmail.com>
2018-05-26 21:03:21 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,6 +6,22 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+def valid_move?(board, index)
+  if(index.between(0, 8) && !position_taken?)
+    return true
+  else
+    return false
+  end
+end
+
+def position_taken?(board, index)
+  if(board[index] == "" || board[index] == " " || board[index] == nil)
+    return false
+  else
+    return true
+  end
+end
+
 def turn(board)
   puts "Please enter 1-9:"
 end
